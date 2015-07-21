7f1daeb64ff45d7b974ee63c78c1ecbae1b023ba
root <root@rhs-cli-14.gdev.lab.eng.bos.redhat.com>
2015-07-21 12:53:44 -0400

    add start/stop vol
    Signed-off-by: root <root@rhs-cli-14.gdev.lab.eng.bos.redhat.com>

--- tier-commands.sh
+++ tier-commands.sh
@@ -6,3 +6,15 @@ function db {
     echo "select * from gf_file_tb;" | sqlite3 $1
 }
 
+function tier_create {
+    gluster v create vol1 rhs-cli-14:/home/t1 rhs-cli-14:/home/t2 force
+    gluster v start vol1
+    yes |gluster v attach-tier vol1 rhs-cli-14:/home/t3 rhs-cli-14:/home/t4 force
+}
+
+function tier_delete {
+    yes|gluster v stop vol1;yes|gluster v delete vol1
+}
+
+
+
