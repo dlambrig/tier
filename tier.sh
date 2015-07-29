fc96dca3c37231d4fa224c555e9ffb717cb63b55
Dan Lambright <dlambrig@redhat.com>
2015-07-29 16:08:36 -0400

    add perf test
    Signed-off-by: Dan Lambright <dlambrig@redhat.com>

--- tier.sh
+++ tier.sh
@@ -1,5 +1,6 @@
 MASTER=rhs-cli-02
 SLAVE=rhs-cli-01
+SLAVE2=rhs-cli-11
 CLIENT=rhs-cli-14
 VOL=vol1
 
@@ -9,6 +10,10 @@ function cleanup {
 
 function cleanup_slave {
     ssh $SLAVE 'for i in {0..8};do rm -rf /home/t$i;mkdir /home/t$i;done'
+}
+
+function cleanup_slave2 {
+    ssh $SLAVE2 'for i in {0..8};do rm -rf /home/t$i;mkdir /home/t$i;done'
 }
 
 function postparms {
@@ -66,13 +71,19 @@ function die {
     killall glusterfsd
     ssh $SLAVE killall glusterfs
     ssh $SLAVE killall glusterfsd
+    ssh $SLAVE2 killall glusterfs
+    ssh $SLAVE2 killall glusterfsd
     rm -rf /var/lib/glusterd
     ssh $SLAVE rm -rf /var/lib/glusterd
+    ssh $SLAVE2 rm -rf /var/lib/glusterd
     systemctl daemon-reload
     systemctl restart glusterd
     ssh $SLAVE systemctl daemon-reload
     ssh $SLAVE systemctl restart glusterd
     gluster peer probe $SLAVE
+    ssh $SLAVE2 systemctl daemon-reload
+    ssh $SLAVE2 systemctl restart glusterd
+    gluster peer probe $SLAVE2
     gluster peer status
 }
 
@@ -81,6 +92,7 @@ function stop {
     yes | gluster v delete $VOL
     cleanup
     cleanup_slave
+    cleanup_slave2
 #    ssh $SLAVE /root/mem-clear.sh
     ssh $CLIENT umount -f /mnt
 }
@@ -88,9 +100,26 @@ function stop {
 function setup  {
     ssh-copy-id $CLIENT
     ssh-copy-id $SLAVE
+    ssh-copy-id $SLAVE2
 }
 
-while getopts ":nsdeatb" opt; do
+function perf1 {
+    gluster v create $VOL disperse 6 redundancy 2 $MASTER:/home/t0 $MASTER:/home/t1 $SLAVE:/home/t0 $SLAVE:/home/t1 $SLAVE2:/home/t0 $SLAVE2:/home/t1 force
+    gluster v start vol1 force
+
+    yes | gluster v attach-tier $VOL replica 2 $MASTER:/home/t2 $MASTER:/home/t3 $SLAVE:/home/t2 $SLAVE:/home/t3 $SLAVE2:/home/t2 $SLAVE2:/home/t3 force
+
+    ssh $CLIENT mount  $MASTER:/$VOL  /mnt
+
+}
+
+function perf2 {
+    gluster v create $VOL disperse 6 redundancy 2 $MASTER:/home/t0 $MASTER:/home/t1 $SLAVE:/home/t0 $SLAVE:/home/t1 $SLAVE2:/home/t0 $SLAVE2:/home/t1 force
+    gluster v start vol1 force
+    ssh $CLIENT mount  $MASTER:/$VOL  /mnt
+}
+
+while getopts ":nsdeatbp" opt; do
   case $opt in
       n)
           echo copy client id
@@ -156,12 +185,28 @@ while getopts ":nsdeatb" opt; do
           echo Done. Now attach
           yes | gluster v attach-tier $VOL replica 2 $SLAVE:/home/t0 $SLAVE:/home/t1 $SLAVE:/home/t2 $SLAVE:/home/t3 force
           ;;
+      p)
+          shift $((OPTIND-1))
+          getopts ":ab" opt
+          case $opt in
+              a)
+                  echo ec+distrep tiered
+                  perf1
+                  ;;
+              b)
+                  echo just ec
+                  perf2
+                  ;;
+              esac
+          ;;
       \?)
           echo "-n : start from scratch: kill restart glusterd"
           echo "-s : stop and remove tiered volume"
           echo "-d : create tiered distributed volume"
           echo "-e : create tiered ec volume"
-          echo "-a : attach volume test"
+          echo "-a : attach volume test (I/O durring attach)"
+          echo "-b : attach volume test (no I/O during attach)"
+          echo "-p : performance test setup"
           ;;
       esac
 done
