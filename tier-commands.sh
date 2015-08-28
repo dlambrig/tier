f19df2fc61c313df000ab78ecd6f1c0951d216bc
Dan Lambright <dlambrig@redhat.com>
2015-08-28 16:53:37 -0400

    Add commands to make an fs
    Signed-off-by: Dan Lambright <dlambrig@redhat.com>

--- tier-commands.sh
+++ tier-commands.sh
@@ -1,4 +1,5 @@
 HOSTNAME=`hostname`
+FREQ=60
 
 alias g=gluster
 
@@ -22,12 +23,35 @@ function tier_create {
     gluster v create vol1 $HOSTNAME:/home/t1 $HOSTNAME:/home/t2 force
     gluster v set vol1 diagnostics.client-log-level DEBUG
     gluster v start vol1
-    yes |gluster v attach-tier vol1 $HOSTNAME:/home/t3 $HOSTNAME:/home/t4 force
+    yes |gluster v tier vol1 attach $HOSTNAME:/home/t3 $HOSTNAME:/home/t4 force
 }
 
 function tier_delete {
     yes|gluster v stop vol1;yes|gluster v delete vol1
 }
 
+function postparms {
+    gluster v set $1 features.ctr-enabled on
+    gluster volume set $1 cluster.read-freq-threshold 0
+    gluster volume set $1 cluster.write-freq-threshold 0
+    gluster volume set $1 cluster.tier-demote-frequency $FREQ
+    gluster volume set $1 cluster.tier-promote-frequency $FREQ
+    gluster volume set $1 diagnostics.client-log-level DEBUG
+}
 
+function make_fs {
+    for i in {1..4};do mkdir /mnt/t$i;done
+    for i in {1..4};do dd if=/dev/zero of=/var/tmp/disk-image$i bs=1M count=20;done
+    for i in {1..4};do mkfs -t xfs -q /var/tmp/disk-image$i;done
+    for i in {1..4};do mount /var/tmp/disk-image$i /mnt/t$i;done
+    gluster v create vol1 $HOSTNAME:/mnt/t1 $HOSTNAME:/mnt/t2 force
+    yes | gluster v attach-tier vol1 $HOSTNAME:/mnt/t3 $HOSTNAME:/mnt/t4 force
+    gluster v start vol1
+    postparms vol1
+}
 
+function kill_fs {
+    tier_delete
+    for i in {1..4};do umount /var/tmp/disk-image$i ;done
+    rm -f /var/tmp/dist-image*
+}
